b095d3758a1909038d4357ae43b56bd14a3f5b17
Daniel Berretta <berretta.daniel@gmail.com>
2022-04-28 11:17:23 -0300

    Login page para registrarse - api problema

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -15,7 +15,7 @@ export function LoginPage() {
 
     //Se envia el formulario con usuario y password.
     //Por el momento se considera como nombre de usuario un email.
-    const handleSubmit = (event) => {
+    const handleLoginSubmit = (event) => {
         event.preventDefault();
         setLoading(true); //estado de loading = true
         setError(""); //borro el error
@@ -44,6 +44,45 @@ export function LoginPage() {
     };
 
 
+
+    //para el register de un nuevo usuario
+    const [emailReg, setEmailReg] = useState("");
+    const [passwordReg, setPasswordReg] = useState("");
+    const [loadingReg, setLoadingReg] = useState(false);
+    const [errorReg, setErrorReg] = useState("");
+
+    //Se envia el formulario con usuario y password.
+    //Por el momento se considera como nombre de usuario un email.
+    const handleSignupSubmit = (event) => {
+        event.preventDefault();
+        setLoadingReg(true); //estado de loading = true
+        setErrorReg(""); //borro el error
+
+        //Llamo a un POST con Axios a /login y mando la informacion del formulario en el estado actual (loading = true)
+        api.post("/auth/register", {email: email, password: password}).then(
+            (response) => {
+                console.log(response);
+                setLoadingReg(false); //estado de loading = false (cambia en caso de exito)
+            },
+            (errorResponse) => {
+                const response = errorResponse.response.data; //guardo la respuesta de error de la API
+                setErrorReg(response.error); //y se cambia el estado para mostrarla
+                setLoadingReg(false); //estado de loading = false (tambien cambia en caso de error)
+            }
+        );
+    };
+    
+    //Para manejar los cambios del formulario, o sea el estado
+    const handleEmailRegChange = (event) => {
+        setEmailReg(event.target.value);
+    };
+
+    const handlePasswordRegChange = (event) => {
+        setPasswordReg(event.target.value);
+    };
+
+
+
     return (
         <>
             <Header titulo = "REFORMAS Y REPARACIONES"
@@ -57,7 +96,7 @@ export function LoginPage() {
                 <BannerSitio />
 
                 {/* Parte derecha superior del grid */}
-                <form className="LoginUsuario" onSubmit={handleSubmit}>
+                <form className="LoginUsuario" onSubmit={handleLoginSubmit}>
                     <h3>Si ya tiene usuario registrado,<br/>ingrese aquÍ</h3>
                     <br/>
 
@@ -92,22 +131,36 @@ export function LoginPage() {
                 </form>
 
                 {/* Parte derecha inferior del grid */}
-                <form className="RegistrarUsuario">
+                <form className="RegistrarUsuario" onSubmit={handleSignupSubmit}>
                     <h3>Si no tiene usuario registrado,<br/>regístrelo con su e-mail</h3>
                     <br/>
 
                     <div>
                         <h2>Usuario nuevo</h2>
-                        <input type="text" name="User"/>
+                        <input
+                            value={emailReg}
+                            onChange={handleEmailRegChange}
+                            type="emailReg"
+                            name="UserReg"
+                        />
                     </div>
 
                     <div>
                         <h2>Contraseña</h2>
-                        <input type="text" name="Pswd"/>
+                        <input
+                            value={passwordReg}
+                            onChange={handlePasswordRegChange}
+                            type="password"
+                            name="PswdReg"
+                        />
                     </div>
                     <br/>
                     
-                    <button>Registrar</button>
+                    <button type="submit">
+                        {loadingReg ? "Registrando ..." : "Registrar"}
+                    </button>
+
+                    <span className="text-danger d-block">{errorReg}</span>
                 </form>
 
                 <Footer />
